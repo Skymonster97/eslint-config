8a6cfa2973c192e7f93cb37edc2d6085fafcc737
Skymonster97 <Skymonster97@gmail.com>
2021-04-17 00:17:26 +0600

    remove overrides

--- rules/json-files.js
+++ rules/json-files.js
@@ -1,21 +1,14 @@
 "use strict";
 
-const overrides = [
-  {
-    "files": [
-      "**/package.json",
-    ],
-    "rules": {
-      "json-files/no-branch-in-dependencies": "off",
-      "json-files/require-engines": "off",
-      "json-files/require-license": "off",
-      "json-files/restrict-ranges": "off",
-      "json-files/sort-package-json": "warn",
-    },
-  },
-];
+const rules = {
+  "json-files/no-branch-in-dependencies": "off",
+  "json-files/require-engines": "off",
+  "json-files/require-license": "off",
+  "json-files/restrict-ranges": "off",
+  "json-files/sort-package-json": "warn",
+};
 
 module.exports = {
   plugins: ["eslint-plugin-json-files"],
-  overrides,
+  rules,
 };
